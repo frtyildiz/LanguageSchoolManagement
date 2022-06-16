b84d69c5dc0313ae711c780dac86b6e8897f9fad
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-17 01:22:11 +0300

    Update ClassroomServiceImpl.java

--- Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/service/ClassroomServiceImpl.java
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/service/ClassroomServiceImpl.java
@@ -1,12 +1,20 @@
 package com.firatyildiz.LanguageSchoolManagement.service;
 
+import com.firatyildiz.LanguageSchoolManagement.repository.ClassroomRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ClassroomServiceImpl {
 
+    private final ClassroomRepository classroomRepository;
 
+    private final ModelMapper modelMapper;
+
+    @Transactional
+    public String saveClassroom ()
 
 }
