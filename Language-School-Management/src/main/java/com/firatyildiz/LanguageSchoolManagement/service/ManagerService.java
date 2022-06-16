e761c2d1e6f47a7d61780ebe4dee767402f3ec59
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-16 06:27:42 +0300

    Update ManagerService.java

--- Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/service/ManagerService.java
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/service/ManagerService.java
@@ -1,9 +1,19 @@
 package com.firatyildiz.LanguageSchoolManagement.service;
 
+import com.firatyildiz.LanguageSchoolManagement.repository.ManagerRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ManagerService {
 
+    @Autowired
+    ManagerRepository managerRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    public String saveManager()
 
 }
