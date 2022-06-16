d13320c759c4502f581fcd97f77f2bb886de27dd
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-16 06:29:21 +0300

    Added SaveManagerRequestDto

---
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/dtos/RequestDto/SaveManagerRequestDto.java
@@ -0,0 +1,23 @@
+package com.firatyildiz.LanguageSchoolManagement.dtos.RequestDto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class SaveManagerRequestDto {
+
+    private int identityCardNo;
+
+    private String name;
+
+    private String lastname;
+
+    private int phoneNumber;
+
+    private String address;
+
+    private LocalDate birthday;
+
+    private String email;
+}
