843ce1ebb919a2a4368c5b3315cc2823da38704b
Fırat Yıldız <62355875+frtyildiz@users.noreply.github.com>
2022-06-16 04:17:45 +0300

    Add files via upload

---
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/LanguageSchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package com.firatyildiz.LanguageSchoolManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageSchoolManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageSchoolManagementApplication.class, args);
+	}
+
+}
