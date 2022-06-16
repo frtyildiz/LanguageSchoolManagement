26632e051ff149761e848bf447ad396277c6b917
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-17 02:14:57 +0300

    Update ClassroomController.java

--- Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/controller/ClassroomController.java
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/controller/ClassroomController.java
@@ -1,14 +1,17 @@
 package com.firatyildiz.LanguageSchoolManagement.controller;
 
+import com.firatyildiz.LanguageSchoolManagement.dtos.RequestDtos.ClassroomRequestDto.AddCourseToClassroomRequestDto;
 import com.firatyildiz.LanguageSchoolManagement.dtos.RequestDtos.ClassroomRequestDto.SaveClassroomRequestDto;
+import com.firatyildiz.LanguageSchoolManagement.dtos.RequestDtos.ClassroomRequestDto.UpdateClassroomRequestDto;
+import com.firatyildiz.LanguageSchoolManagement.dtos.ResponseDto.ClassroomResponseDto;
+import com.firatyildiz.LanguageSchoolManagement.entity.Classroom;
 import com.firatyildiz.LanguageSchoolManagement.service.ClassroomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Classroom")
@@ -25,7 +28,45 @@ public class ClassroomController {
         return new ResponseEntity<>(saveText, HttpStatus.OK);
     }
 
-    
+    @GetMapping("/findClassroom")
+    public ResponseEntity<Classroom> findClassroomById (@RequestParam long classroomId)
+    {
+        Classroom classroom = classroomService.findClassroomById(classroomId);
+
+        return new ResponseEntity<>(classroom, HttpStatus.OK);
+    }
+
+    @PostMapping("/updateClassroom")
+    public ResponseEntity<String> updateClassroom (@RequestBody UpdateClassroomRequestDto updateClassroomRequestDto)
+    {
+        String updateText = classroomService.updateClassroom(updateClassroomRequestDto);
+
+        return new ResponseEntity<>(updateText, HttpStatus.OK);
+    }
+
+    @PostMapping("/addCourseToClassroom")
+    public ResponseEntity<String> addCourseToClassroom (@RequestBody AddCourseToClassroomRequestDto addCourseToClassroomRequestDto)
+    {
+        String addCourseText = classroomService.addCourseToClassroomById(addCourseToClassroomRequestDto);
+
+        return new ResponseEntity<>(addCourseText, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deleteClassroom")
+    public ResponseEntity<String> deleteClassroom (@RequestParam long classroomId)
+    {
+        String deleteText = classroomService.deleteClassroomById(classroomId);
+
+        return new ResponseEntity<>(deleteText, HttpStatus.OK);
+    }
+
+    @GetMapping("/findAllClassroom")
+    public ResponseEntity<List<ClassroomResponseDto>> findAllCourse ()
+    {
+        List<ClassroomResponseDto> classroomResponseDtos = classroomService.findAllClassroom();
+
+        return new ResponseEntity<>(classroomResponseDtos, HttpStatus.OK);
+    }
 
 
 }
