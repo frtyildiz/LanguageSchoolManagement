bb7140a8406321c9b72906529a96a551e8e944dc
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-17 02:05:26 +0300

    Update ClassroomController.java

--- Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/controller/ClassroomController.java
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/controller/ClassroomController.java
@@ -1,6 +1,12 @@
 package com.firatyildiz.LanguageSchoolManagement.controller;
 
+import com.firatyildiz.LanguageSchoolManagement.dtos.RequestDtos.ClassroomRequestDto.SaveClassroomRequestDto;
+import com.firatyildiz.LanguageSchoolManagement.service.ClassroomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/Classroom")
 @RequiredArgsConstructor
 public class ClassroomController {
+
+    private final ClassroomService classroomService;
+
+    @PostMapping("/saveClassroom")
+    private ResponseEntity<String> saveClassroom(@RequestBody SaveClassroomRequestDto saveClassroomRequestDto)
+    {
+        String saveText = classroomService.saveClassroom(saveClassroomRequestDto);
+
+        return new ResponseEntity<>(saveText, HttpStatus.OK);
+    }
 
     
 
