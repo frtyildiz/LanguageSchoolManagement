a3c1dfa059f1b7e5a0c8d05916c86cedb9f05d0a
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-17 06:19:01 +0300

    Update TeacherServiceImpl.java

--- Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/service/TeacherServiceImpl.java
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/service/TeacherServiceImpl.java
@@ -1,6 +1,9 @@
 package com.firatyildiz.LanguageSchoolManagement.service;
 
+import com.firatyildiz.LanguageSchoolManagement.repository.CourseRepository;
+import com.firatyildiz.LanguageSchoolManagement.repository.TeacherRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +11,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class TeacherServiceImpl {
+
+    private final TeacherRepository teacherRepository;
+
+    private final CourseRepository courseRepository;
+
+    private final ModelMapper modelMapper;
+
+    public String saveTeacher ()
+
+
+
+
+
+
+
 }
