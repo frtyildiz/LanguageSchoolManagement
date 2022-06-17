916abd358aafa85b90eeaf72a69f00606d1a6cdd
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-17 04:42:54 +0300

    Update StudentServiceImpl.java

--- Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/service/StudentServiceImpl.java
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/service/StudentServiceImpl.java
@@ -2,12 +2,15 @@ package com.firatyildiz.LanguageSchoolManagement.service;
 
 
 import com.firatyildiz.LanguageSchoolManagement.dtos.RequestDtos.StudentRequestDto.SaveStudentRequestDto;
+import com.firatyildiz.LanguageSchoolManagement.dtos.RequestDtos.StudentRequestDto.UpdateStudentRequestDto;
 import com.firatyildiz.LanguageSchoolManagement.entity.Student;
 import com.firatyildiz.LanguageSchoolManagement.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -32,7 +35,19 @@ public class StudentServiceImpl {
         return studentRepository.findById(studentId).get();
     }
 
-    public String updateStudentById
+    public String updateStudentById (UpdateStudentRequestDto updateStudentRequestDto)
+    {
+        long updateStudentRequestId = updateStudentRequestDto.getId();
+
+        Optional<Student> studentOptional = studentRepository.findById(updateStudentRequestId);
+        Student student = studentOptional.get();
+
+        student = modelMapper.map(updateStudentRequestDto, Student.class);
+
+        studentRepository.save(student);
+
+        return "Changes Saved.";
+    }
 
 
 }
