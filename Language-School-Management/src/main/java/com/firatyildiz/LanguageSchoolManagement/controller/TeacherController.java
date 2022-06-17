6adb5269b7d5789c8ddd807fa09826ae1fa8bbe7
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-17 06:14:58 +0300

    Added TeacherController

---
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/controller/TeacherController.java
@@ -0,0 +1,11 @@
+package com.firatyildiz.LanguageSchoolManagement.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teacher")
+@RequiredArgsConstructor
+public class TeacherController {
+}
