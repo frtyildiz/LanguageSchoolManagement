2ff1703dd81b0423d5461e363c52f61e537af622
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-17 06:35:02 +0300

    Create TeacherResponseDto.java

---
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/dtos/ResponseDto/TeacherResponseDto.java
@@ -0,0 +1,32 @@
+package com.firatyildiz.LanguageSchoolManagement.dtos.ResponseDto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class TeacherResponseDto {
+
+    private long id;
+
+    private long identityCardNumber;
+
+    private String name;
+
+    private String surname;
+
+    private int salary;
+
+    private String major;
+
+    private int phoneNo;
+
+
+    private String address;
+
+
+    private LocalDate birthday;
+
+
+    private String email;
+}
