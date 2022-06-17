a30b2f2f1fe7ff5e72380de619125e0c34da69c9
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-17 06:39:50 +0300

    Update TeacherServiceImpl.java

--- Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/service/TeacherServiceImpl.java
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/service/TeacherServiceImpl.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class TeacherServiceImpl {
+public class TeacherServiceImpl implements TeacherService {
 
     private final TeacherRepository teacherRepository;
 
