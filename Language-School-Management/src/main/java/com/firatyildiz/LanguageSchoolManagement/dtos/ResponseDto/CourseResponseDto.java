e91656e7b37adf10f362bd56a30771ba887a6175
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-18 09:19:09 +0300

    Create CourseResponseDto.java

---
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/dtos/ResponseDto/CourseResponseDto.java
@@ -0,0 +1,32 @@
+package com.firatyildiz.LanguageSchoolManagement.dtos.ResponseDto;
+
+import com.firatyildiz.LanguageSchoolManagement.entity.Classroom;
+import com.firatyildiz.LanguageSchoolManagement.entity.Teacher;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+public class CourseResponseDto {
+
+    private long id;
+
+
+    private String courseName;
+
+
+    private String courseLevel;
+
+
+    private LocalDate startDate;
+
+
+    private LocalDate endDate;
+
+    
+    private Classroom classroom;
+
+
+    private Teacher teacher;
+
+}
