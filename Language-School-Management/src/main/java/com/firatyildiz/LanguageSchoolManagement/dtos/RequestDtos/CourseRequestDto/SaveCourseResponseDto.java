1bb233f6b97dc019923013cd08303a8a63c8ba44
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-18 08:56:45 +0300

    Create SaveCourseResponseDto.java

---
+++ Language-School-Management/src/main/java/com/firatyildiz/LanguageSchoolManagement/dtos/RequestDtos/CourseRequestDto/SaveCourseResponseDto.java
@@ -0,0 +1,41 @@
+package com.firatyildiz.LanguageSchoolManagement.dtos.RequestDtos.CourseRequestDto;
+
+import com.firatyildiz.LanguageSchoolManagement.entity.Classroom;
+import com.firatyildiz.LanguageSchoolManagement.entity.Student;
+import com.firatyildiz.LanguageSchoolManagement.entity.Teacher;
+import lombok.Data;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class SaveCourseResponseDto {
+
+    private long id;
+
+
+    private String courseName;
+
+
+    private String courseLevel;
+
+
+    private LocalDate startDate;
+
+
+    private LocalDate endDate;
+
+
+    private byte[] materials;
+
+
+    private Classroom classroom;
+
+
+    private Teacher teacher;
+
+
+    private List<Student> students = new ArrayList<>();
+}
