7f52364bf3c78d342e3d3a3c9a771b294911e062
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-06-19 08:58:32 +0300

    Updated ManagerServiceImplTest

--- Language-School-Management/src/test/java/com/firatyildiz/LanguageSchoolManagement/service/ManagerServiceImplTest.java
+++ Language-School-Management/src/test/java/com/firatyildiz/LanguageSchoolManagement/service/ManagerServiceImplTest.java
@@ -1,28 +1,107 @@
 package com.firatyildiz.LanguageSchoolManagement.service;
 
+import com.firatyildiz.LanguageSchoolManagement.dtos.RequestDtos.ManagerRequestDto.SaveManagerRequestDto;
+import com.firatyildiz.LanguageSchoolManagement.dtos.RequestDtos.ManagerRequestDto.UpdateManagerRequestDto;
+import com.firatyildiz.LanguageSchoolManagement.entity.Manager;
+import com.firatyildiz.LanguageSchoolManagement.repository.ManagerRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class ManagerServiceImplTest {
 
+    @InjectMocks
+    ManagerServiceImpl managerService;
+
+    @Mock
+    ManagerRepository managerRepository;
+
+    @Mock
+    ModelMapper modelMapper;
     @Test
-    void saveManager() {
+    void testSaveManager()
+    {
+        SaveManagerRequestDto saveManagerRequestDtoMock = mock(SaveManagerRequestDto.class);
+        saveManagerRequestDtoMock.setName("test");
+        saveManagerRequestDtoMock.setLastname("test");
+
+        Manager managerMock = mock(Manager.class);
+        managerMock.setId(1);
+        managerMock.setIdentityCardNo(123);
+        managerMock.setName("testName");
+        managerMock.setLastname("testLastname");
+
+        when(modelMapper.map(saveManagerRequestDtoMock, Manager.class)).thenReturn(managerMock);
+        when(managerRepository.save(managerMock)).thenReturn(managerMock);
+
+        String saveManager = managerService.saveManager(saveManagerRequestDtoMock);
+        String saveManagerMessage = "Manager Has Been Created.";
+
+        assertEquals(saveManagerMessage, saveManager);
     }
 
     @Test
-    void findManagerById() {
+    void testFindManagerById()
+    {
+        Manager managerMock = mock(Manager.class);
+        managerMock.setId(1);
+        managerMock.setIdentityCardNo(123);
+        managerMock.setName("testName");
+        managerMock.setLastname("testLastname");
+
+        when(managerRepository.findById(managerMock.getId())).thenReturn(Optional.of(managerMock));
+        Manager findManager = managerService.findManagerById(managerMock.getId());
+
+        assertEquals(managerMock, findManager);
     }
 
     @Test
-    void updateManagerById() {
+    void testUpdateManagerById()
+    {
+        UpdateManagerRequestDto updateManagerRequestDtoMock = mock(UpdateManagerRequestDto.class);
+        updateManagerRequestDtoMock.setId(1);
+        updateManagerRequestDtoMock.setName("test");
+        updateManagerRequestDtoMock.setLastname("test");
+
+        Manager managerMock = mock(Manager.class);
+        managerMock.setId(1);
+        managerMock.setIdentityCardNo(123);
+        managerMock.setName("testName");
+        managerMock.setLastname("testLastname");
+
+        when(managerRepository.findById(updateManagerRequestDtoMock.getId())).thenReturn(Optional.of(managerMock));
+        when(modelMapper.map(updateManagerRequestDtoMock, Manager.class)).thenReturn(managerMock);
+        when(managerRepository.save(managerMock)).thenReturn(managerMock);
+
+        String updateManager = managerService.updateManagerById(updateManagerRequestDtoMock);
+        String updateManagerMessage = "Changes Saved.";
+
+        assertEquals(updateManagerMessage, updateManager);
     }
 
     @Test
-    void deleteManagerById() {
-    }
+    void testDeleteManagerById()
+    {
+        Manager managerMock = mock(Manager.class);
+        managerMock.setId(1);
+        managerMock.setIdentityCardNo(123);
+        managerMock.setName("testName");
+        managerMock.setLastname("testLastname");
 
-    @Test
-    void findAllManager() {
+        when(managerRepository.findById(managerMock.getId())).thenReturn(Optional.of(managerMock));
+        String deleteManager = managerService.deleteManagerById(managerMock.getId());
+        String deleteManagerMessage = "The Manager Deleted.";
+
+        assertEquals(deleteManagerMessage, deleteManager);
     }
 }
